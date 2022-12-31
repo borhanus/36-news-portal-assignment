2146e246e4b33f95001b4b8485477f9dffc5564b
borhanus <borhan.u10@gmail.com>
2022-12-31 13:37:15 +0600

    trying to add today's pick

--- script.js
+++ script.js
@@ -55,8 +55,6 @@ const categoryItems = async (ID, name) => {
 }
 
 // Category Items publish
-let todaysPickArray = [];
-let todaysTrendingArray = [];
 const categoryItemsDetails = items => {
     loadding(false);
 
@@ -111,18 +109,7 @@ const categoryItemsDetails = items => {
         
         `
         newsPosts.appendChild(div);
-
-        if (item.others_info.is_todays_pick) {
-            todaysPickArray.push(item._id);
-        }
-        if (item.others_info.is_trending) {
-            todaysTrendingArray.push(item._id);
-        }
     });
-    const todaysPick = document.getElementById('todaysPick');
-    todaysPick.setAttribute('onclick', `test()`)
-    const todaysTrending = document.getElementById('todaysTrending');
-    todaysPick.setAttribute('onclick', `test()`)
 }
 
 // Modal
@@ -213,16 +200,6 @@ const postPublisher = (items) => {
     });
 }
 
-
-
-
-
-
-
-
-
-
-
 // Items Found alert
 const alert = () => {
     const alert = document.getElementById('categoryAlert');
@@ -245,110 +222,136 @@ categoryItems(08);
 
 
 
-// let todaysPickArray = [];
-// let todaysTrendingArray = [];
+// // let todaysPickArray = [];
+// // let todaysTrendingArray = [];
 
-// Trending / Today's Pick
-const test = () => {
-    const newsPosts = document.getElementById('newsPosts');
-    newsPosts.innerHTML = ``;
-    if (todaysPickArray.length === 1) {
-        todaysPickArray.forEach(data => {
-            todaysTrendingFetch(data);
-        })
-    }
-    if (todaysTrendingArray.length === 19) {
-        todaysTrendingArray.forEach(data => {
-            todaysTrendingFetch(data);
-        })
-    }
+// // Trending / Today's Pick
 
-    console.log(todaysTrendingArray.length);
-}
 
-const todaysTrendingFetch = async (ID) => {
-    loadding(true);
-    try {
-        const res = await fetch(`https://openapi.programming-hero.com/api/news/${ID}`);
-        const data = await res.json();
-        todaysTrendingPosts(data.data)
-    }
-    catch (error) {
-        console.error(error);
-    }
-}
+// const todaysTrendingFetch = async (ID) => {
+//     loadding(true);
+//     try {
+//         const res = await fetch(`https://openapi.programming-hero.com/api/news/${ID}`);
+//         const data = await res.json();
+//         todaysTrendingPosts(data.data)
+//     }
+//     catch (error) {
+//         console.error(error);
+//     }
+// }
 
-const todaysTrendingPosts = items => {
-    loadding(false);
-    const numberOfCategory = document.getElementById('numberOfCategory');
-    numberOfCategory.innerText = items.length;
-    
-    items.forEach(item => {
-        const div = document.createElement('div');
-        div.classList.add('card', 'mb-3');
-        div.innerHTML =
-            `
-        <div class="row g-0">
-            <div class="col-md-3 card-body d-flex justify-content-center">
-                <img src="${item.thumbnail_url}"
-                    class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-9 d-grid align-content-between">
-                <div class="card-body">
-                    <h5 class="card-title fs-3">${item.title}</h5>
-                    <p class="card-text text-muted">${item.details.slice(0, 300)}</p>
-                    <br>
-                    <p class="card-text text-muted">${item.details.slice(300, 400) ? item.details.slice(300, 400) + ' . . .' : ''}</p>
-                </div>
-                <div class="d-flex justify-content-between align-items-center card-body">
-                    <div class="d-flex gap-2 align-items-center">
-                        <img class="rounded-circle" src="${item.author.img}" alt="" height="40" width="40">
-                        <ul class="list-no m-0 p-0">
-                            <li class="fw-semibold">${item.author.name ? item.author.name : 'unavailable'}</li>
-                            <li class="text-muted fw-semibold fs-6"><small>${item.author.published_date ? item.author.published_date.slice(0, 10) : 'unavailable'}</small></li>
-                        </ul>
-                    </div>
-                    <div>
-                        <i class="fa-regular fa-eye"></i>
-                        <span class="ps-2 fw-semibold">${item.total_view ? item.total_view : 'unavailable'}</span>
-                    </div>
-                    <ul class="d-flex m-0 p-0 gap-3">
-                        <li><i class="fa-regular fa-star-half-stroke"></i></li>
-                        <li><i class="fa-regular fa-star"></i></li>
-                        <li><i class="fa-regular fa-star"></i></li>
-                        <li><i class="fa-regular fa-star"></i></li>
-                        <li><i class="fa-regular fa-star"></i></li>
-                    </ul>
-                    <div class="pe-3">
-                        <button onclick="modalCallForNews('${item._id}')" type="button" class="fa-solid fa-arrow-right fs-2 btn text-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        `
-        newsPosts.appendChild(div);
+// const todaysTrendingPosts = items => {
+//     loadding(false);
+//     const numberOfCategory = document.getElementById('numberOfCategory');
+//     numberOfCategory.innerText = items.length;
 
-    });
-}
-// const todaysPick = [];
-// const trending = [];
-// const trendingPicksCategoryItemsDetails = (items) => {
 //     items.forEach(item => {
-//         if (item.others_info.is_todays_pick || item.others_info.is_trending) {
-//             todaysPick.push(item._id);
-//             console.log(item._id);
-//             trending.push(item._id);
-//             console.log(item._id + 'aaaaaaaaaaaaaa551');
+//         const div = document.createElement('div');
+//         div.classList.add('card', 'mb-3');
+//         div.innerHTML =
+//             `
+//         <div class="row g-0">
+//             <div class="col-md-3 card-body d-flex justify-content-center">
+//                 <img src="${item.thumbnail_url}"
+//                     class="img-fluid rounded-start" alt="...">
+//             </div>
+//             <div class="col-md-9 d-grid align-content-between">
+//                 <div class="card-body">
+//                     <h5 class="card-title fs-3">${item.title}</h5>
+//                     <p class="card-text text-muted">${item.details.slice(0, 300)}</p>
+//                     <br>
+//                     <p class="card-text text-muted">${item.details.slice(300, 400) ? item.details.slice(300, 400) + ' . . .' : ''}</p>
+//                 </div>
+//                 <div class="d-flex justify-content-between align-items-center card-body">
+//                     <div class="d-flex gap-2 align-items-center">
+//                         <img class="rounded-circle" src="${item.author.img}" alt="" height="40" width="40">
+//                         <ul class="list-no m-0 p-0">
+//                             <li class="fw-semibold">${item.author.name ? item.author.name : 'unavailable'}</li>
+//                             <li class="text-muted fw-semibold fs-6"><small>${item.author.published_date ? item.author.published_date.slice(0, 10) : 'unavailable'}</small></li>
+//                         </ul>
+//                     </div>
+//                     <div>
+//                         <i class="fa-regular fa-eye"></i>
+//                         <span class="ps-2 fw-semibold">${item.total_view ? item.total_view : 'unavailable'}</span>
+//                     </div>
+//                     <ul class="d-flex m-0 p-0 gap-3">
+//                         <li><i class="fa-regular fa-star-half-stroke"></i></li>
+//                         <li><i class="fa-regular fa-star"></i></li>
+//                         <li><i class="fa-regular fa-star"></i></li>
+//                         <li><i class="fa-regular fa-star"></i></li>
+//                         <li><i class="fa-regular fa-star"></i></li>
+//                     </ul>
+//                     <div class="pe-3">
+//                         <button onclick="modalCallForNews('${item._id}')" type="button" class="fa-solid fa-arrow-right fs-2 btn text-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+//                         </button>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+        
+//         `
+//         newsPosts.appendChild(div);
+
+//     });
+// }
+
+// const todayPickCategoryItems = async () => {
+//     loadding(true);
+//     try {
+//         const res = await fetch(`https://openapi.programming-hero.com/api/news/category/08`);
+//         const data = await res.json();
+//         todayPickCategoryItemsDetails(data.data);
+//         alert();
+//         return data.data;
+//     }
+//     catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// const todaysPick = () => {
+//     const newsPosts = document.getElementById('newsPosts');
+//     newsPosts.innerHTML = ``;
+//     if (todaysPickArray.length === 1) {
+//         todaysPickArray.forEach(data => {
+//             todaysTrendingFetch(data);
+//         })
+//     }
+
+
+//     console.log(todaysPickArray.length);
+// }
+
+// const todaysTrending = () => {
+//     const newsPosts = document.getElementById('newsPosts');
+//     newsPosts.innerHTML = ``;
+//     if (todaysTrendingArray.length === 19) {
+//         todaysTrendingArray.forEach(data => {
+//             todaysTrendingFetch(data);
+//         })
+//     }
+
+//     console.log(todaysTrendingArray.length);
+// }
+
+
+// const todayPickCategoryItemsDetails = (items) => {
+//     let todaysPickArray = [];
+//     let todaysTrendingArray = [];
+//     items.forEach(item => {
+//         if (item.others_info.is_todays_pick) {
+//             todaysPickArray.push(item._id);
+//         }
+//         if (item.others_info.is_trending) {
+//             todaysTrendingArray.push(item._id);
 //         }
 //     })
+//     console.log(todaysPickArray);
+//     console.log(todaysTrendingArray);
+    
 // }
-// console.log(todaysPick);
-// console.log(trending);
 
-// categoryItemsDetails(todaysPick);
-// categoryItemsDetails(trending);
+
 const todayTrendings = document.querySelectorAll('#todayTrending button');
 todayTrendings.forEach(todayTrending => {
     todayTrending.addEventListener('click', function (event) {
@@ -356,30 +359,14 @@ todayTrendings.forEach(todayTrending => {
             todayTrending.classList.remove('my-active');
         })
         event.target.classList.add('my-active');
-        // trendingPicksCategoryItems(08, `${event.target.innerText}`)
+        // if (event.target.innerText === `Today's Pick`) {
+        //     todayPickCategoryItems();
+        // }
+        // else if (event.target.innerText === 'Trending') {
+        //     todayPickCategoryItems();
+        // }
     })
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
